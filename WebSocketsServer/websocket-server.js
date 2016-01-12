31ed872dbef6034acedbad4d4430ee33a0c73c74
alexlerikos <alerikos@gmail.com>
2016-01-12 02:11:31 -0800

    Added velocity calibration and output to IPython notebook

--- WebSocketsServer/websocket-server.js
+++ WebSocketsServer/websocket-server.js
@@ -18,6 +18,7 @@ var wss = new WebSocketServer({
     port: 8000
 });
 
+// console.log("Server ip: ", String(wss.host));
 //Sleep function for AnalogRead delay
 
 function sleep(clock) { //in milliseconds
@@ -35,6 +36,7 @@ wss.on('connection', function(ws) {
 
     // Send message to client that connection has been made.
     ws.send('Connection Successful');
+    console.log("Received connection");
 
     // Handle incoming messages.
     ws.on('message', function(message) {
